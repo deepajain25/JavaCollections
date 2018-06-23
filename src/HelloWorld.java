0b9006c3f3135d974cdaba5ecb86967d1a34b943
Deepa Jain <jaindeepa01@gmail.com>
2018-06-23 18:10:47 +0530

    test file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,36 @@
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         ConcurrentHashMap hashMap = new ConcurrentHashMap();
+        hashMap.put("1","1");
+        hashMap.put("2","1");
+        System.out.println(hashMap.hashCode());
+        Iterator it  = hashMap.keySet().iterator();
         hashMap.put("1","deepa");
+        String a = new String("deepa");
+        System.out.println(a.hashCode());
+        String b = a;
+        System.out.println(b.hashCode());
+        a = "ghjk";
+        System.out.println(a.hashCode());
+        System.out.println(a + " " + b);
+
+        ConcurrentHashMap hashMap1 = hashMap;
+        hashMap1.put("1","3");
+        System.out.println(hashMap1.hashCode());
+
+        CopyOnWriteArrayList list = new CopyOnWriteArrayList();
+
+        list.add("123");
+        list.add("456");
+        Iterator it1= list.iterator();
+        System.out.println("************************");
 
     }
 }
